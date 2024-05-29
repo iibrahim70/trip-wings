085bf660a60bbb83cde14fa9fa8420f044f96b55
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-29 21:40:11 +0600

    added static breadcrumbs

--- src/components/FlightDetailsCard.tsx
+++ src/components/FlightDetailsCard.tsx
@@ -1,14 +1,20 @@
+import { IoIosArrowForward } from "react-icons/io";
+
 const FlightDetailsCard = () => {
   return (
     <section className="w-3/4">
-      <p>Home</p>
-      <p>Booking History</p>
-      <p>Flight</p>
-      <p className="text-blue-500">STFL17121182045413</p>
-
-      <div className="shadow rounded-md p-5 border h-full">
-        FlightDetailsCard
+      {/* static breadcrumbs */}
+      <div className="cursor-pointer flex items-center gap-1.5 pb-3 text-gray-500">
+        <p>Home</p>
+        <IoIosArrowForward />
+        <p>Booking History</p>
+        <IoIosArrowForward />
+        <p>Flight</p>
+        <IoIosArrowForward />
+        <p className="text-blue-500 font-semibold">STFL17121182045413</p>
       </div>
+
+      <div className="shadow rounded-md p-5 border">FlightDetailsCard</div>
     </section>
   );
 };
