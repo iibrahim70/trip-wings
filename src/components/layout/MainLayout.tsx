4aea3491d9d553193fafa39ee8d38d4e1626d5b5
iibrahim70 <iibrahiim.dev@gmail.com>
2024-05-29 20:51:47 +0600

    added basic nav

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../shared/Navbar";
+
+const MainLayout = () => {
+  return (
+    <main>
+      <Navbar />
+
+      
+      MainLayout
+    </main>
+  );
+};
+
+export default MainLayout;
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="shadow-md py-3.5">
+      <h1 className="section-wrapper font-bold text-xl">Trip Wings</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
